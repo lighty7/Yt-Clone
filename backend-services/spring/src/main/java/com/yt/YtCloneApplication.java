100f5aea477e7793e1650601946f3d73000b1d62
manju_adur <manjunathadur2001@gmail.com>
2025-09-27 11:50:56 +0530

    feat: add backend service folder for java

---
+++ backend-services/spring/src/main/java/com/yt/YtCloneApplication.java
@@ -0,0 +1,13 @@
+package com.yt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtCloneApplication.class, args);
+	}
+
+}
---
+++ backend-services/spring/src/test/java/com/yt/YtCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
